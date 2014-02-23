3ec25a2ac35e3d71f273e5fc1e69ffe9fc0ef44f
Subbu Balakrishnan <subbuthepeaceful@gmail.com>
2014-02-23 09:12:56 -0800

    Initial commit of Rails 3 strikezone with ActiveAdmin and Devise

---
+++ strikezone/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Strikezone::Application.config.secret_token = '30ac51fa27b8c5b50a99febc32f1f6ec72f48b7f832897246cc49ccf00f70643564fd9b084f5b9985340a232dee62227fa5f428b50ab07e2baafdd16aaa26943'

279dd5bc4ba2593e91de6913c7d51e1cd7d44af0
Subbu Balakrishnan <subbuthepeaceful@gmail.com>
2014-03-31 06:15:11 -0700

    Removed rvmrc and custom logger

--- strikezone/lib/custom_logger.rb
+++
@@ -1,9 +0,0 @@
-class CustomLogger < Logger
-  def format_message(severity, timestamp, progname, msg)
-    "#{msg}\n"
-  end
-end
-
-logfile = File.open(Rails.root + 'log/custom.log', 'a')
-logfile.sync
-CUSTOM_LOGGER = CustomLogger.new(logfile)
